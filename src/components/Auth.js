ea4e501a0033cd8185a35f295c2d1959d97d3902
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-05-03 20:22:18 +0530

    dispatch login function

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/auth-slice";
 
 import "./Auth.css";
 
 const Auth = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // dispatch action
+    const dispatch = useDispatch();
+    dispatch(authActions.login);
+  };
   return (
     <div className="container">
-      <h1>Login</h1>{" "}
-      <form>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="id">Id</label>
         <input type="text" name="id" id="id" />
         <label htmlFor="password">Password</label>
