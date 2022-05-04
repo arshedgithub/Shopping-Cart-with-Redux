452270bd032f522a63dbdb6575f126156f176ba9
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-05-05 02:25:39 +0530

    connect with firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import "./App.css";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   console.log(isLoggedIn);
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      const res = await fetch(
+        "https://redux-shopping-cart-a8a49-default-rtdb.firebaseio.com/cartItems.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      const data = await res.json();
+    };
+    sendRequest();
+  }, [cart]);
+
   return (
     <div className="App">
       {!isLoggedIn && <Auth />}
